bea587e4d7a4c18a6161314974a16d3c5365a159
piio-zicko <60844821+piio-zicko@users.noreply.github.com>
2022-03-22 13:05:46 +0100

    Building a c# calculator console project

---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿using thirdproject.Maths;
+
+namespace ThirdProject
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string restart_calculator = "no";
+            string[] operator_array = new string[5] { "+", "-", "/", "%", "*" };
+            int number1;
+            int number2;
+
+            do
+            {
+                Console.WriteLine("What do you want to calculate? (+, -, *, %, /)");
+                string type_of_calculation = Console.ReadLine();
+
+                while (!operator_array.Contains(type_of_calculation))
+                {
+                    Console.WriteLine("choose a valid operator!");
+                    type_of_calculation = Console.ReadLine();
+                    Console.WriteLine("Enter your first number");
+
+                    string input = Console.ReadLine();
+                    while (int.TryParse(input, out number1) == false)
+                    {
+                        Console.WriteLine("Ohh! an error occured, try again!");
+                        input = Console.ReadLine();
+                    }
+                    number1 = Int32.Parse(input);
+                    Console.WriteLine("Enter your second number");
+                    input = Console.ReadLine();
+                    while (int.TryParse(input, out number2) == false)
+                    {
+                        Console.WriteLine("An error occured, try again with a number :P");
+                        input = Console.ReadLine();
+                    }
+                    number2 = Int32.Parse(input);
+
+                    if (type_of_calculation == "+")
+                    {
+                        Console.WriteLine("The Answer is " + Calculator.Add(number1, number2));
+                        Console.WriteLine("type 'yes' to restart the calculation ");
+                        restart_calculator = Console.ReadLine();
+                    }
+                    if (type_of_calculation == "-") ;
+                    {
+                        Console.WriteLine("The Answer is " + Calculator.Substraction(number1, number2));
+                        Console.WriteLine("type 'yes' to restart the calculation ");
+                        restart_calculator = Console.ReadLine();
+                    }
+                    if (type_of_calculation == "*")
+                    {
+                        Console.WriteLine("The Answer is " + Calculator.Multiplication(number1, number2));
+                        Console.WriteLine("type 'yes' to restart the calculation ");
+                        restart_calculator = Console.ReadLine();
+                    }
+                    if (type_of_calculation == "/")
+                    {
+                        Console.WriteLine("The Answer is " + Calculator.Division(number1, number2));
+                        Console.WriteLine("type 'yes' to restart the calculation ");
+                        restart_calculator = Console.ReadLine();
+                    }
+                    else if (type_of_calculation == "%")
+                    {
+                        Console.WriteLine("The Answer is " + Calculator.equoficient(number1, number2));
+                        Console.WriteLine("type 'yes' to restart the calculation ");
+                        restart_calculator = Console.ReadLine();
+                    }
+                }
+
+            } while (restart_calculator != null);
+        }
+    }
+}         
+
+
+
+                    /*
+                    Console.WriteLine("How old are you?");
+                    string age = Console.ReadLine();
+                    var sum = Int32.Parse(age);
+                    var sum2 = int.Parse(age);
+                    int newAge = sum + 10;
+                    Console.WriteLine("Your age is 10 years will be " + newAge);
+
+
+                   var name = new Person();
+                    name.firstName = "Andrew";
+                    name.lastName = "Wassers";
+                    name.Introduce();
+                }
+
+              Calculator sums = new Calculator();
+            var problem = sums.Add(300, 7);
+            Console.WriteLine(problem);
+            Calculator volume = new Calculator();
+            var solution = volume.Multiplication(400, 3);
+            Console.WriteLine(solution);
+                    */
---
+++ calculator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thirdproject.Maths
+{
+    public class Calculator
+    {
+        public static int Add( int a, int b)
+        {
+            int number1 = a;
+            int number2 = b;
+            int answer = number1 + number2;
+            return a + b;
+        }
+        public static int Substraction(int a , int b)
+        {
+            int number1 = a;
+            int number2 = b;
+            int answer = number1 - number2;
+            return a - b; 
+        }
+        public static float Division(int a, int b)
+        {
+            float number1 = a;
+            float number2 = b;
+            float answer = number1 / number2;
+            return answer; 
+        }
+        public static int Multiplication(int a, int b)
+        {
+            int number1 = a;
+            int number2 = b;
+            int answer = number1 * number2;
+            return a * b;
+        }
+        public static int equoficient(int a, int b)
+        {
+            int number1 = a;
+            int number2 = b;
+            int answer = number1 % number2;
+            return a * b;
+        }
+    }
+}
